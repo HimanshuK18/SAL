5cbf75edf5871c242f36bf61cb242a6b88cce19d
HimanshuK18 <himanshukhurana18@gmail.com>
2018-03-01 10:26:16 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 // config/db.js
     module.exports = {
-        url : 'mongodb://54.172.10.34:27017/LOC',
+        url : 'mongodb://35.154.149.187:27017/LOC',
         urlIot : 'mongodb://32.227.56.250:27017/admin'
                 //  mongodb://32.227.56.250:27017/admin
     }
